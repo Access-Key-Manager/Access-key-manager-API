519ae15cc35a31024f0fb148bc194b59f535e52b
ValHammond <137390662+ValHammond@users.noreply.github.com>
2023-07-19 10:53:24 +0100

    feat: Set the email field as the username field for the User model

--- users/models.py
+++ users/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     # todo: add other fields that are not on AbstractUser
+    email = models.EmailField(_("Email Address"), blank=False, null=False, unique=True)
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     def __str__(self) -> str:
         return self.email
